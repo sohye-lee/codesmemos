d01b211097a7e8fbaa46ebda1d983fdf4d41764e
sohye-lee <sohyelee@gmail.com>
2024-01-08 23:58:17 -0500

    post routes

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,3 +1,13 @@
+import { db } from '@/db';
 import { NextRequest } from 'next/server';
 
-export async function GET(req: NextRequest, context: any) {}
+export async function GET(req: NextRequest, context: any) {
+  const posts = await db.post.findMany({
+    include: {
+      user: true,
+      topic: true,
+      comments: true,
+      saves: true,
+    },
+  });
+}
