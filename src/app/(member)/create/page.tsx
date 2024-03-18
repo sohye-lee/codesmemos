fb22200f96ffb3f677062cfd76661451a7568703
Sohye Lee <sohyelee@gmail.com>
2024-03-18 12:32:30 -0400

    background color fix

--- src/app/(member)/create/page.tsx
+++ src/app/(member)/create/page.tsx
@@ -1,16 +1,16 @@
-'use client';
-import Container from '@/components/ui/containers/container';
-import { postType } from '@/lib/types';
-import { useEffect, useState } from 'react';
-import { useForm } from 'react-hook-form';
-import CreateSnippetForm from '@/components/forms/createSnippetForm';
-import { useSearchParams } from 'next/navigation';
-import CreateResourceForm from '@/components/forms/createResourceForm';
-import CreateQuestionForm from '@/components/forms/createQuestionForm';
-import useStore from '../../store';
-import { useTheme } from 'next-themes';
+"use client";
+import Container from "@/components/ui/containers/container";
+import { postType } from "@/lib/types";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import CreateSnippetForm from "@/components/forms/createSnippetForm";
+import { useSearchParams } from "next/navigation";
+import CreateResourceForm from "@/components/forms/createResourceForm";
+import CreateQuestionForm from "@/components/forms/createQuestionForm";
+import useStore from "../../store";
+import { useTheme } from "next-themes";
 
-type TabType = 'snippet' | 'question' | 'resource';
+type TabType = "snippet" | "question" | "resource";
 
 interface CreatePostForm {
   title: string;
@@ -28,16 +28,16 @@ export default function CreatePage(props: any) {
   const [bgColor, setBgColor] = useState<string>();
   const { breadcrumb, setBreadcrumb } = useStore();
   const searchParams = useSearchParams();
-  const type = searchParams.get('type')?.toString();
-  const [tab, setTab] = useState(type || 'snippet');
+  const type = searchParams.get("type")?.toString();
+  const [tab, setTab] = useState(type || "snippet");
 
-  const activeStyle = 'border-b-none bg-background';
-  const inactiveStyle = 'border-b border-slate-400 bg-gray-200 text-gray-600';
-  const inactiveDarkStyle = 'border-b border-slate-400 bg-gray-700 ';
+  const activeStyle = "border-b-none bg-background";
+  const inactiveStyle = "border-b border-slate-400 bg-gray-200 text-gray-600";
+  const inactiveDarkStyle = "border-b border-slate-400 bg-gray-700 ";
 
   useEffect(() => {
-    theme == 'light' ? setBgColor('bg-blue-100') : setBgColor('');
-    setBreadcrumb('create');
+    theme == "light" ? setBgColor("bg-blue-100") : setBgColor("bg-background");
+    setBreadcrumb("create");
     type && type != null && setTab(type);
   }, [setBreadcrumb, type, searchParams]);
 
@@ -49,45 +49,45 @@ export default function CreatePage(props: any) {
           <div className="w-full flex items-stretch">
             <div
               className={`w-1/3 py-2 px-3 text-md  ${
-                tab == 'snippet'
+                tab == "snippet"
                   ? activeStyle
-                  : theme == 'dark'
+                  : theme == "dark"
                   ? inactiveDarkStyle
                   : inactiveStyle
               }`}
-              onClick={() => setTab('snippet')}
+              onClick={() => setTab("snippet")}
             >
               Snippet
             </div>
             <div
               className={`w-1/3 py-2 px-3  text-md border-l border-slate-400 ${
-                tab == 'question'
+                tab == "question"
                   ? activeStyle
-                  : theme == 'dark'
+                  : theme == "dark"
                   ? inactiveDarkStyle
                   : inactiveStyle
               }`}
-              onClick={() => setTab('question')}
+              onClick={() => setTab("question")}
             >
               Question
             </div>
             <div
               className={`w-1/3 py-2 px-3  text-md border-l border-slate-400 ${
-                tab == 'resource'
+                tab == "resource"
                   ? activeStyle
-                  : theme == 'dark'
+                  : theme == "dark"
                   ? inactiveDarkStyle
                   : inactiveStyle
               }`}
-              onClick={() => setTab('resource')}
+              onClick={() => setTab("resource")}
             >
               Resource
             </div>
           </div>
           <div className="p-4 bg-background">
-            {tab == 'snippet' && <CreateSnippetForm />}
-            {tab == 'question' && <CreateQuestionForm />}
-            {tab == 'resource' && <CreateResourceForm />}
+            {tab == "snippet" && <CreateSnippetForm />}
+            {tab == "question" && <CreateQuestionForm />}
+            {tab == "resource" && <CreateResourceForm />}
           </div>
         </div>
       </div>
